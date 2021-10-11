4fd1fadba37fa1b6f694928e55e7cf3227310c57
Ashu Aryan <ashuaryan.jsr@gmail.com>
2021-10-12 00:20:28 +0530

    football

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import "./App.css";
+import JSONDATA from './PlayersData.json'
+import { useState } from "react" 
+function App(){
+  const [searchTerm, setSearchTerm] = useState("")
+  return(
+    <div className = "App">(
+    <input type = "text" placeholder = "Search" onChange = {event => {setSearchTerm(event.target.value)}}/>
+    {JSONDATA.filter((val) =>{
+      if(searchTerm===""){
+        return val
+      }else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())){
+        return val.first_name
+      }
+    }).map((val,key) => {
+      return( 
+      <div className = "user" key={key}>
+        <p> {val.first_name} </p>
+        </div>
+      );
+    })}
+     </div>
   );
 }
-
 export default App;
